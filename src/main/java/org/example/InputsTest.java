022d53fab82223e4c60d54be0bf31e9dd954a602
goodr <jack.suvorov13@gmail.com>
2022-07-06 22:52:08 +0300

    Initial commit

---
+++ src/main/java/org/example/InputsTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class InputsTest extends BaseTest {
+
+    @Test
+    public void inputsTest() {
+        driver.findElement(By.linkText("Inputs")).click();
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys(Keys.ARROW_UP);
+
+        driver.navigate().back();
+    }
+}
